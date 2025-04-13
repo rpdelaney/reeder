d4b6d1857c61f350dac67dd678bdeb349fad94ed
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2025-04-13 19:47:02 -0400

    Initial

---
+++ reed/__main__.py
@@ -0,0 +1,29 @@
+"""Do the business."""
+
+import fileinput
+import sys
+
+import httpx
+import yt_dlp
+from bs4 import BeautifulSoup as Bs
+from pdftotext import PDF
+from readability import Document
+from yt_dlp.utils import DownloadError
+
+import urllib.parse
+
+
+
+def main() -> int:
+    """Texitfy a URL."""
+    with fileinput.input() as data:
+        if not data:
+            return 1
+        for line in data:
+            print(line)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
