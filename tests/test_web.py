9e439826bee49f8f98a63fa27fdd8ace423f9d93
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2025-04-13 21:56:25 -0400

    Add tests for fetch happy path

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,8 +1,12 @@
+from http import HTTPStatus
+from unittest.mock import MagicMock, call
+
+import httpx
 import hypothesis.provisional as pstrats
 import hypothesis.strategies as strats
 from hypothesis import given
 
-from reed.web import is_url
+from reed.web import fetch, is_url
 
 
 @given(pstrats.urls())
@@ -13,3 +17,22 @@ def test_is_url(url):
 @given(strats.text())
 def test_is_not_url(noturl):
     assert not is_url(noturl)
+
+
+class TestFetch:
+    @given(
+        url_str=pstrats.urls(),
+        timeout=strats.integers(min_value=1, max_value=10),
+    )
+    def test_fetch_success(self, url_str, timeout):
+        client = MagicMock(httpx.Client)
+        client.get.return_value = httpx.Response(HTTPStatus.OK, content=b"OK")
+        url = httpx.URL(url_str)
+
+        response = fetch(client, httpx.URL(url), timeout)
+
+        assert client.method_calls == [
+            call.get(url=url, timeout=timeout, follow_redirects=True)
+        ]
+        assert response.status_code == HTTPStatus.OK
+        assert response.content == b"OK"
