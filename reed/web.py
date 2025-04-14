e6c17e8e9fa0f1e26d571bbd4a946d72f8a77c75
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2025-04-13 21:38:25 -0400

    Add pytest-mock

--- reed/web.py
+++ reed/web.py
@@ -3,6 +3,7 @@
 from urllib.parse import urlparse
 
 import deal
+import httpx
 
 
 deal.activate()
@@ -14,3 +15,12 @@ def is_url(string: str) -> bool:
     """Return whether the string is a URL."""
     result = urlparse(string)
     return all([result.scheme, result.netloc])
+
+
+def fetch(client: httpx.Client, url: httpx.URL, timeout: int = 10):
+    """Get the content from a page at a URL."""
+    return client.get(
+        url=url,
+        timeout=timeout,
+        follow_redirects=True,
+    )
