304094f1d094a536eab78234d9af17ba96eb3fdb
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2025-04-24 20:48:03 -0400

    Remove contracted exceptions that we can't actually raise

--- reed/web.py
+++ reed/web.py
@@ -11,24 +11,6 @@ deal.module_load(deal.pure)
 
 
 @deal.has()
-@deal.raises(ValueError, TypeError)
-@deal.reason(
-    ValueError,
-    lambda string: string == "https://foobarbaz:-a",
-    message="""
-        urlparse() can raise ValueError if an invalid port
-        is specified in the URL.
-    """,
-)
-@deal.reason(
-    TypeError,
-    lambda _: False,
-    message="""
-        urlparse() raises TypeError if a single function call mixes
-        str and non-str arguments. However, since this function uses only one
-        argument to urlparse, this condition cannot be met.
-    """,
-)
 def is_url(string: str) -> bool:
     """Return whether the string is a URL.
 
