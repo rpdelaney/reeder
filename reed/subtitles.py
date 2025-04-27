fb4a62f93c525ee9216a851e640731b8e0b34b23
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2025-04-27 17:01:30 -0400

    Simplify extractor detection with a generator

--- reed/subtitles.py
+++ reed/subtitles.py
@@ -8,9 +8,9 @@ from yt_dlp.extractor import gen_extractors  # type: ignore[attr-defined]
 @deal.pure
 def has_extractor(url: httpx.URL) -> bool:
     """Check if the provided URL is supported by any yt_dlp extractor."""
-    for extractor in gen_extractors():
+    return any(
         # The generic extractor always returns suitable, even
-        # when it won't run on the URL
-        if extractor.suitable(str(url)) and extractor.IE_NAME != "generic":
-            return True
-    return False
+        # when it won't run on the URL.
+        extractor.suitable(str(url)) and extractor.IE_NAME != "generic"
+        for extractor in gen_extractors()
+    )
