3f97c7c17c33025c4775e29d487ff0c30d640c3d
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2025-04-29 17:11:46 -0400

    Add get_subtitles function

--- reed/subtitles.py
+++ reed/subtitles.py
@@ -1,8 +1,14 @@
 """Fetch video subtitles from a remote source."""
 
+import contextlib
+import tempfile
+from pathlib import Path
+
 import deal
 import httpx
+import yt_dlp
 from yt_dlp.extractor import gen_extractors  # type: ignore[attr-defined]
+from yt_dlp.utils import DownloadError
 
 
 @deal.pure
@@ -14,3 +20,47 @@ def has_extractor(url: httpx.URL) -> bool:
         extractor.suitable(str(url)) and extractor.IE_NAME != "generic"
         for extractor in gen_extractors()
     )
+
+
+@deal.raises(DownloadError)
+def get_subtitles(url: httpx.URL) -> bytes:
+    """Extract automatic subtitles from a video URL using yt_dlp."""
+    if not has_extractor(url):
+        return b""
+
+    class YtLogger:
+        """Don't log anything from yt_dlp except errors."""
+
+        def debug(self, msg: str) -> None:
+            pass
+
+        def warning(self, msg: str) -> None:
+            pass
+
+        def error(self, msg: str) -> None:
+            print(msg)
+
+    temp_dir = tempfile.TemporaryDirectory()
+    ydl_opts = {
+        "skip_download": True,
+        "writeautomaticsub": True,
+        "subtitleslangs": ["en"],
+        "subtitlesformat": "vtt",
+        "logger": YtLogger(),
+        "outtmpl": str(Path(temp_dir.name) / Path("%(id)s.%(ext)s")),
+        "noplaylist": True,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:  # type: ignore[arg-type]
+        info_dict = ydl.extract_info(str(url))
+        video_id = info_dict.get("id")
+        subs_filename = Path(temp_dir.name) / Path(f"{video_id}.en.vtt")
+
+    subtitles = ""
+    with (
+        contextlib.suppress(FileNotFoundError),
+        Path.open(subs_filename, encoding="utf-8") as file,
+    ):
+        subtitles = file.read()
+
+    return subtitles.encode() if subtitles else b""
