04f15fc49f8f2cf89ac2e11343798c8ce9f3a7c5
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2025-04-29 17:22:34 -0400

    Use get_subtitles if URL has a yt_dlp extractor

--- reed/__main__.py
+++ reed/__main__.py
@@ -5,8 +5,10 @@ import sys
 import click
 import deal
 import httpx
+from yt_dlp.utils import DownloadError
 
 from reed.content import ContentRenderer
+from reed.subtitles import get_subtitles
 from reed.web import fetch, is_url
 
 
@@ -18,6 +20,7 @@ deal.module_load(deal.pure)
 @deal.raises(
     RuntimeError,
     NotImplementedError,
+    DownloadError,
     httpx.RequestError,
     httpx.TimeoutException,
 )
@@ -26,12 +29,16 @@ deal.module_load(deal.pure)
 def main(target: str) -> int:
     """Texitfy content found at TARGET. TARGET can be a URL or a file path."""
     if is_url(target):
-        webclient = httpx.Client()
+        if subtitles := get_subtitles(httpx.URL(target)):
+            renderer = ContentRenderer(data=subtitles)
+            content_type = "text/plain"
+        else:
+            webclient = httpx.Client()
 
-        response = fetch(webclient, httpx.URL(target))
+            response = fetch(webclient, httpx.URL(target))
 
-        renderer = ContentRenderer(data=response.content)
-        content_type = response.headers.get("Content-Type").split(";")[0]
+            renderer = ContentRenderer(data=response.content)
+            content_type = response.headers.get("Content-Type").split(";")[0]
     else:
         msg = (
             "TARGET must be a URL accessible via networking. "
