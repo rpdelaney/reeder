e1e6039cae294a5881150565a655d022d0c7a431
Ryan Delaney <ryan.patrick.delaney+git@protonmail.com>
2025-06-09 12:36:44 -0400

    Add more contracts for content handling strip behavior

--- reed/content.py
+++ reed/content.py
@@ -1,6 +1,7 @@
 """Provide content handling."""
 
 import io
+import string
 
 import deal
 import readability
@@ -28,32 +29,50 @@ class ContentRenderer:
         return self._data
 
     @deal.pure
+    @deal.post(lambda result: not result.startswith(tuple(string.whitespace)))
+    @deal.post(lambda result: not result.endswith(tuple(string.whitespace)))
     def text(self) -> str:
         r"""Render text from... text.
 
-        >>> r = ContentRenderer(b"aaa\xff\xfe\xfd")
+        >>> r = ContentRenderer(b"\taaa\xff\xfe\xfd\t \n ")
         >>> r.text()
         'aaa���'
         """
-        return self._data.decode(encoding="utf-8", errors="replace")
+        return self._data.decode(encoding="utf-8", errors="replace").strip(
+            string.whitespace
+        )
 
     @deal.pure
+    @deal.post(lambda result: not result.startswith(tuple(string.whitespace)))
+    @deal.post(lambda result: not result.endswith(tuple(string.whitespace)))
     def html(self) -> str:
         """Render text from HTML."""
         soup = Bs(self._data, "html.parser")
-        return soup.get_text().strip()
+        return soup.get_text().strip(string.whitespace)
 
     @deal.pure
+    @deal.post(
+        lambda result: all(
+            not term.startswith(tuple(string.whitespace)) for term in result
+        )
+    )
+    @deal.post(
+        lambda result: all(
+            not term.endswith(tuple(string.whitespace)) for term in result
+        )
+    )
     def html_readable(self) -> tuple[str, str]:
         """Render text from HTML, using python-readability."""
         readable_doc = readability.parse(self.text())
 
-        title: str = readable_doc.title.strip()
-        summary: str = readable_doc.text_content.strip()
+        title: str = readable_doc.title.strip(string.whitespace)
+        summary: str = readable_doc.text_content.strip(string.whitespace)
 
         return title, summary
 
     @deal.pure
+    @deal.post(lambda result: not result.startswith(tuple(string.whitespace)))
+    @deal.post(lambda result: not result.endswith(tuple(string.whitespace)))
     def pdf(self) -> str:
         """Render text from PDF."""
-        return extract_text(io.BytesIO(self._data)).strip()
+        return extract_text(io.BytesIO(self._data)).strip(string.whitespace)
